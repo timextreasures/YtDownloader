46a873fd7453524f1829314ad2824df15522733b
Jonathansty <jonathan.steyfkens@telenet.be>
2015-08-10 00:53:44 +0200

    ytDownloader-GUI
    Started GUI development

---
+++ ytDownloader-GUI/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ytDownloader_GUI
+{
+    partial class printingBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_addmp3 = new System.Windows.Forms.Button();
+            this.button_addmp4 = new System.Windows.Forms.Button();
+            this.list_queue = new System.Windows.Forms.ListView();
+            this.txtBox_link = new System.Windows.Forms.TextBox();
+            this.label_title = new System.Windows.Forms.Label();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_download = new System.Windows.Forms.Button();
+            this.button_selectDir = new System.Windows.Forms.Button();
+            this.save_download = new System.Windows.Forms.FolderBrowserDialog();
+            this.download_progress = new System.Windows.Forms.ProgressBar();
+            this.progress_label = new System.Windows.Forms.Label();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.downloader = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button_addmp3
+            // 
+            this.button_addmp3.Location = new System.Drawing.Point(10, 48);
+            this.button_addmp3.Name = "button_addmp3";
+            this.button_addmp3.Size = new System.Drawing.Size(77, 23);
+            this.button_addmp3.TabIndex = 1;
+            this.button_addmp3.Text = "add mp3";
+            this.button_addmp3.UseVisualStyleBackColor = true;
+            this.button_addmp3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_addmp4
+            // 
+            this.button_addmp4.Location = new System.Drawing.Point(93, 48);
+            this.button_addmp4.Name = "button_addmp4";
+            this.button_addmp4.Size = new System.Drawing.Size(77, 23);
+            this.button_addmp4.TabIndex = 2;
+            this.button_addmp4.Text = "add mp4";
+            this.button_addmp4.UseVisualStyleBackColor = true;
+            this.button_addmp4.Click += new System.EventHandler(this.button_addmp4_Click);
+            // 
+            // list_queue
+            // 
+            this.list_queue.Location = new System.Drawing.Point(176, 48);
+            this.list_queue.Name = "list_queue";
+            this.list_queue.Size = new System.Drawing.Size(332, 240);
+            this.list_queue.TabIndex = 3;
+            this.list_queue.UseCompatibleStateImageBehavior = false;
+            this.list_queue.View = System.Windows.Forms.View.List;
+            this.list_queue.SelectedIndexChanged += new System.EventHandler(this.list_queue_SelectedIndexChanged);
+            // 
+            // txtBox_link
+            // 
+            this.txtBox_link.Location = new System.Drawing.Point(143, 12);
+            this.txtBox_link.Name = "txtBox_link";
+            this.txtBox_link.Size = new System.Drawing.Size(365, 20);
+            this.txtBox_link.TabIndex = 4;
+            this.txtBox_link.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Location = new System.Drawing.Point(12, 15);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(125, 13);
+            this.label_title.TabIndex = 5;
+            this.label_title.Text = "Input a valid youtube link";
+            this.label_title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(12, 294);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(158, 23);
+            this.button_clear.TabIndex = 6;
+            this.button_clear.Text = "Clear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button_download
+            // 
+            this.button_download.Location = new System.Drawing.Point(10, 106);
+            this.button_download.Name = "button_download";
+            this.button_download.Size = new System.Drawing.Size(160, 23);
+            this.button_download.TabIndex = 7;
+            this.button_download.Text = "Download";
+            this.button_download.UseVisualStyleBackColor = true;
+            this.button_download.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button_selectDir
+            // 
+            this.button_selectDir.Location = new System.Drawing.Point(10, 135);
+            this.button_selectDir.Name = "button_selectDir";
+            this.button_selectDir.Size = new System.Drawing.Size(160, 23);
+            this.button_selectDir.TabIndex = 8;
+            this.button_selectDir.Text = "Select Download directory";
+            this.button_selectDir.UseVisualStyleBackColor = true;
+            this.button_selectDir.Click += new System.EventHandler(this.button1_Click_3);
+            // 
+            // save_download
+            // 
+            this.save_download.HelpRequest += new System.EventHandler(this.save_download_HelpRequest);
+            // 
+            // download_progress
+            // 
+            this.download_progress.Location = new System.Drawing.Point(176, 294);
+            this.download_progress.Name = "download_progress";
+            this.download_progress.Size = new System.Drawing.Size(305, 23);
+            this.download_progress.TabIndex = 9;
+            this.download_progress.Click += new System.EventHandler(this.download_progress_Click);
+            // 
+            // progress_label
+            // 
+            this.progress_label.AutoSize = true;
+            this.progress_label.Location = new System.Drawing.Point(484, 299);
+            this.progress_label.Name = "progress_label";
+            this.progress_label.Size = new System.Drawing.Size(24, 13);
+            this.progress_label.TabIndex = 10;
+            this.progress_label.Text = "0/0";
+            this.progress_label.Visible = false;
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(10, 77);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(160, 23);
+            this.button_delete.TabIndex = 11;
+            this.button_delete.Text = "delete";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // downloader
+            // 
+            this.downloader.DoWork += new System.ComponentModel.DoWorkEventHandler(this.downloader_DoWork);
+            // 
+            // printingBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 329);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.progress_label);
+            this.Controls.Add(this.download_progress);
+            this.Controls.Add(this.button_selectDir);
+            this.Controls.Add(this.button_download);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.label_title);
+            this.Controls.Add(this.txtBox_link);
+            this.Controls.Add(this.list_queue);
+            this.Controls.Add(this.button_addmp4);
+            this.Controls.Add(this.button_addmp3);
+            this.Name = "printingBox";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_addmp3;
+        private System.Windows.Forms.Button button_addmp4;
+        private System.Windows.Forms.ListView list_queue;
+        private System.Windows.Forms.TextBox txtBox_link;
+        private System.Windows.Forms.Label label_title;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_download;
+        private System.Windows.Forms.Button button_selectDir;
+        private System.Windows.Forms.FolderBrowserDialog save_download;
+        private System.Windows.Forms.ProgressBar download_progress;
+        private System.Windows.Forms.Label progress_label;
+        private System.Windows.Forms.Button button_delete;
+        private System.ComponentModel.BackgroundWorker downloader;
+    }
+}
+
---
+++ ytDownloader-GUI/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace ytDownloader_GUI
+{
+    enum type
+    {
+        MP3,
+        MP4,
+        None,
+    }
+    struct linkInfo
+    {
+        public string link;
+        public type downloadType;
+    }
+
+    public partial class printingBox : Form
+    {
+        private string downloadDir = "Download/";
+        
+        public printingBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string link = txtBox_link.Text;
+            
+
+            var items = list_queue.Items;
+            ListViewItem item = null;
+            string compoundString = "mp3 " + txtBox_link.Text;
+            
+            if (!isFound(compoundString,items))
+            {
+                item = list_queue.Items.Add(compoundString);
+            }
+            try
+            {
+                using (var client = new MyClient())
+                {
+                    client.HeadOnly = true;
+                    string s1 = client.DownloadString(link);
+                }
+            }
+            catch (Exception ek)
+            {
+                MessageBox.Show(ek.Message);
+                list_queue.Items.Remove(item);
+                return;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void list_queue_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_addmp4_Click(object sender, EventArgs e)
+        {
+            string link = txtBox_link.Text;
+            var items = list_queue.Items;
+            ListViewItem item = null;
+            string compoundString = "mp4 " + txtBox_link.Text;
+            if (!isFound(compoundString, items))
+            {
+                item = list_queue.Items.Add(compoundString);
+            }
+            
+            try
+            {
+                using (var client = new MyClient())
+                {
+                    client.HeadOnly = true;
+                    string s1 = client.DownloadString(link);
+                }
+            }
+            catch (Exception ek)
+            {
+                MessageBox.Show(ek.Message);
+                list_queue.Items.Remove(item);
+                return;
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            list_queue.Items.Clear();
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            var items = list_queue.Items;
+            int amount = items.Count;
+            progress_label.Visible = true;
+            for (int i = 0; i < items.Count; i++)
+            {
+                string input = items[i].Text;
+                string type = input.Substring(0, input.IndexOf(" "));
+                type actualType = toType(type);
+
+                string link = input.Substring(type.Length+1);
+
+                linkInfo info;
+                info.link = link;
+                info.downloadType = actualType;
+                progress_label.Text = i+1 + "/" + amount;
+                progress_label.Update();
+                Download(info, downloadDir).Wait();
+                
+                
+            }
+            items.Clear();
+            progress_label.Visible = false;
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+            save_download.ShowDialog();
+            downloadDir = save_download.SelectedPath;
+        }
+
+        private void save_download_HelpRequest(object sender, EventArgs e)
+        {
+            
+        }
+
+        bool isFound(string compoundString, ListView.ListViewItemCollection items)
+        {
+            bool found = false;
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].Text == compoundString)
+                {
+                    found = true;
+                }
+            }
+            return found;
+        }
+
+        static type toType(string input)
+        {
+            if (input.ToLower().Equals("mp3"))
+            {
+                return type.MP3;
+            }
+            else if (input.ToLower().Equals("mp4"))
+            {
+                return type.MP4;
+            }
+            return type.None;
+        }
+
+        async Task Download(linkInfo info, string dir)
+        {
+            switch (info.downloadType)
+            {
+                case type.MP3:
+                    DownloadMP3(info.link, dir);
+                    break;
+                case type.MP4:
+                    DownloadMP4(info.link, dir);
+                    break;
+                case type.None:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void DownloadMP3(string link, string downloadDir)
+        {
+            Directory.CreateDirectory(downloadDir);
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+
+            // Extracting stream with highest quality
+            VideoInfo video = videoInfos.Where(info => info.CanExtractAudio).OrderByDescending(info => info.AudioBitrate).First();
+
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            var audioDownloader = new AudioDownloader(video, Path.Combine(downloadDir, RemoveIllegalPathCharacters(video.Title) + video.AudioExtension));
+            audioDownloader.DownloadProgressChanged += (sender, argss) =>
+            {
+                download_progress.Value = (int)argss.ProgressPercentage;
+
+            };
+            audioDownloader.DownloadFinished += (sender, argss) =>
+            {
+                download_progress.Value = 0;
+                Console.WriteLine("\nFinished download " + video.Title + video.AudioExtension);
+            };
+
+            audioDownloader.Execute();
+        }
+
+        void DownloadMP4(string link, string downloadDir)
+        {
+            Directory.CreateDirectory(downloadDir);
+
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+
+            VideoInfo video = videoInfos.First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 720);
+
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            var videoDownloader = new VideoDownloader(video, Path.Combine(downloadDir, RemoveIllegalPathCharacters(video.Title) + video.VideoExtension));
+            videoDownloader.DownloadStarted += (sender, argss) => Console.WriteLine("Started downloading " + video.Title + video.VideoExtension);
+
+            videoDownloader.DownloadProgressChanged += (sender, argss) =>
+            {
+                download_progress.Value = (int)argss.ProgressPercentage;
+
+            };
+            videoDownloader.DownloadFinished += (sender, argss) => download_progress.Value = 0;
+
+            videoDownloader.Execute();
+        }
+        private static string RemoveIllegalPathCharacters(string path)
+        {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(path, "");
+        }
+
+        private void download_progress_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            var items = list_queue.SelectedItems;
+            ListView.ListViewItemCollection list = list_queue.Items;
+            list.Clear();
+            int count = items.Count;
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(items[i]);
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                list_queue.Items.Remove(list[i]);
+            }
+        }
+
+        private void downloader_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+        }
+    }
+
+    class MyClient : WebClient
+    {
+        public bool HeadOnly { get; set; }
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest req = base.GetWebRequest(address);
+            if (HeadOnly && req.Method == "GET")
+            {
+                req.Method = "HEAD";
+            }
+            return req;
+        }
+    }
+}
